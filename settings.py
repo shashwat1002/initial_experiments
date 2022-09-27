5ed67b380c71294ee8a1ae1c09d22524f84aeeec
Shashwat Singh <singhshashwat1002@gmail.com>
2022-09-27 14:28:54 +0530

    settings

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
 CONFIGURATION = BertConfig(output_hidden_states=True)
 CONFIGURATION.hidden_size = HIDDEN_SIZE
 FREEZE_BERT = True
-NUM_EPOCHS = 100
+NUM_EPOCHS = 500
 BATCH_SIZE = 50
 DEVICE = "cuda"

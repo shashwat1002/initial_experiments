795367abee20008a518c862178b1ebd8e7d0ac92
Shashwat Singh <singhshashwat1002@gmail.com>
2022-10-16 12:14:51 +0530

    add a learning rate setting (and change model path)

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
 CONFIGURATION = BertConfig(output_hidden_states=True)
 CONFIGURATION.hidden_size = HIDDEN_SIZE
 FREEZE_BERT = True
-NUM_EPOCHS = 250
+NUM_EPOCHS = 200
 BATCH_SIZE = 1000
 DEVICE = "cuda"
 
@@ -14,4 +14,5 @@ TRAIN_FILE_PATH = "lama_train.txt"
 TEST_FILE_PATH = "lama_test.txt"
 DEV_FILE_PATH = "lama_dev.txt"
 BERT_INPUT_SIZE = 128
-MODEL_PATH = "model.pth"
+MODEL_PATH = "model2.pth"
+LEARNING_RATE = 1e-5

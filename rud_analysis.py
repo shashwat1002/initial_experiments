a298d67c87258f8a0dc678aefc2d7eb95389f019
Shashwat Singh <singhshashwat1002@gmail.com>
2022-11-25 15:35:50 +0530

    replace all magic numbers with LAYER_NUM

--- rud_analysis.py
+++ rud_analysis.py
@@ -4,13 +4,15 @@ from scipy.signal import savgol_filter
 import matplotlib as mpl
 from icecream import ic
 
+LAYER_NUM = 13
+
 def accuracy_analysis(file_path):
 
     with open(file_path, "r") as out_data:
 
         accuracy_line_count = 0
-        layer_train_accuracy_data = [[] for i in range(12)]
-        layer_test_accuracy_data = [[] for i in range(12)]
+        layer_train_accuracy_data = [[] for i in range(LAYER_NUM)]
+        layer_test_accuracy_data = [[] for i in range(LAYER_NUM)]
 
         main_data = [layer_train_accuracy_data, layer_test_accuracy_data]
         # this is because train and test reports alternate
@@ -23,7 +25,7 @@ def accuracy_analysis(file_path):
 
             try:
                 if line_parsed[1] == "accuracy":
-                    main_data[int(accuracy_line_count / 12) % 2][accuracy_line_count % 12].append(float(line_parsed[2]))
+                    main_data[int(accuracy_line_count / LAYER_NUM) % 2][accuracy_line_count % LAYER_NUM].append(float(line_parsed[2]))
                     accuracy_line_count += 1
             except IndexError:
                 pass
@@ -33,12 +35,12 @@ def accuracy_analysis(file_path):
         ic(len(layer_train_accuracy_data[0]))
         return layer_train_accuracy_data, layer_test_accuracy_data
 
-train_rep, test_rep = accuracy_analysis("test_result_two.txt")
+train_rep, test_rep = accuracy_analysis("test_output_roberta_proper_1.txt")
 # print(data)
 
 def plot_function(data, name):
     mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple', 'pink', 'brown', 'orange', 'teal', 'coral', 'lightblue', 'lime', 'lavender', 'turquoise', 'darkgreen', 'tan', 'salmon', 'gold'])
-    fig, ax = pyplot.subplots(3)
+    fig, ax = pyplot.subplots(4)
     for index, row in enumerate(data):
         ax[int(index/4)].plot(range(len(row)), row, label=f"{index+1}")
     for i in range(3):
@@ -56,9 +58,10 @@ def smooth_plot(data, name):
     ax.legend()
     pyplot.savefig(f'figures/{name}.png')
 
-plot_function(train_rep, "train_subplots")
-plot_function(test_rep, "test_subplots")
+pyplot.ylim([0.48, 0.75])
+plot_function(train_rep, "train_roberta_proper_1")
+plot_function(test_rep, "test_roberta_proper_1")
 
-smooth_plot(train_rep, "train_smooth")
-smooth_plot(test_rep, "test_smooth")
+smooth_plot(train_rep, "train_roberta_proper_sooth_1")
+smooth_plot(test_rep, "test_roberta_proper_sooth_1")
 

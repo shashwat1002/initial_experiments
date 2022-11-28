cb7e9806f8816e45d8e8d763918b56c7b31bbae3
Shashwat Singh <singhshashwat1002@gmail.com>
2022-11-28 20:05:46 +0530

    make the control dataset and run the actual task
    make test function accept models that aren't distributed

--- control.py
+++ control.py
@@ -16,11 +16,20 @@ def run_gib_control_task(model_path):
     dataset1 = data.NegLamaDataet(TRAIN_FILE_PATH, BERT_INPUT_SIZE)
     dataset2 = data.NegLamaDataet(TEST_FILE_PATH, BERT_INPUT_SIZE)
 
-    model = model_setup.ExperimentModel(CONFIGURATION)
+    model = model_setup.ExperimentModel(CONFIGURATION).to(DEVICE)
     model.load_state_dict(model_path)
 
+    print("Normal run")
     train.test_model(dataset1, model=model)
     train.test_model(dataset2, model=model)
+
+    dataset1 = data.NegLamaDataet(TRAIN_FILE_PATH, BERT_INPUT_SIZE, control_task=True)
+    dataset2 = data.NegLamaDataet(TEST_FILE_PATH, BERT_INPUT_SIZE, control_task=True)
+
+    print("Control run")
+    train.test_model(dataset1, model=model)
+    train.test_model(dataset2, model=model)
+
 
 def main():
     opt_vals, opt_left = getopt.getopt(sys.argv[1:], longopts=["model_file="])

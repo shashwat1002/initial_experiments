a2abe6e2ffe475da5d96865d0d5d33c711982db5
Shashwat Singh <singhshashwat1002@gmail.com>
2022-12-31 00:46:42 +0530

    code and results for the A prior experiment

--- neg_lm.py
+++ neg_lm.py
@@ -7,6 +7,8 @@ import json
 from tabulate import tabulate
 from torch.nn import Softmax
 from torch.distributions import Categorical
+from icecream import ic
+# ic.disable()
 
 
 """
@@ -119,7 +121,7 @@ def test_model(model, dataset):
     ic(pred_tokens2)
 
 
-def deal_with_pair(model, input1):
+def deal_with_pair(model, input1, sentence3=None):
 
     k = 20
 
@@ -159,6 +161,109 @@ def deal_with_pair(model, input1):
 
     return out_string
 
+def compare_n_sentences(model, sentence_list, correct_pred):
+    k = 20
+
+    masked_logits_list = [send_mask_logits(model, sentence) for sentence in sentence_list]
+
+    softmax_func = Softmax()
+    preds_prob_list = [softmax_func(masked_logits.squeeze()) for masked_logits in masked_logits_list]
+
+    top_k_preds_list = []
+    top_k_indices_list = []
+
+    for preds_prob in preds_prob_list:
+        top_k_preds, top_k_indices = top_k(k, preds_prob)
+        top_k_preds_list.append(top_k_preds)
+        top_k_indices_list.append(top_k_indices)
+
+    categorical_preds = [Categorical(preds_prob) for preds_prob in preds_prob_list]
+    entropies = [categorical_pred.entropy() for categorical_pred in categorical_preds]
+
+    ## this is specific to the AAbar experiment
+    change = (
+                (not (top_k_indices_list[0].squeeze()[0] == top_k_indices_list[1].squeeze()[0])),
+                (not (top_k_indices_list[0].squeeze()[0] == top_k_indices_list[2].squeeze()[0])),
+                (not (top_k_indices_list[1].squeeze()[0] == top_k_indices_list[2].squeeze()[0])),
+                )
+
+    pred_tokens = [TOKENIZER.convert_ids_to_tokens(indices.squeeze()) for indices in top_k_indices_list]
+
+    out_string = ""
+
+    for sentence in sentence_list:
+        out_string += f"{sentence}\n"
+    for chan in change:
+        out_string += f"{str(chan)}\n"
+    for entropy in entropies:
+        out_string += f"{entropy}, "
+    out_string += f"{correct_pred}\n"
+
+    data = []
+
+    for i in range(k):
+        entry = []
+        for j in range(len(sentence_list)):
+            entry.append(pred_tokens[j][i])
+            entry.append(top_k_preds_list[j].squeeze()[i])
+        data.append(entry)
+
+    out_string += f"{tabulate(data)}"
+
+    return out_string
+
+def deal_with_pair_expt2(model, inp):
+    """
+    input will have "A <mask>" and <A' mask>
+    - We will first create "A <pred>"
+    - We will then make A <pred>. <sep> <A'> <mask>
+    - Compare against A <mask>. predictions and A' <mask> predictions
+    """
+    k = 20
+    # completing A<mask>
+    masked_logits1 = send_mask_logits(model, inp[0])
+    softmax_func = Softmax()
+    preds1_prob = softmax_func(masked_logits1.squeeze())
+    preds1, indices1 = top_k(k, preds1_prob)
+    top_pred = indices1[0]
+    tok_list1 = TOKENIZER(inp[0]).input_ids
+    mask1_index = tok_list1.index(TOKENIZER.mask_token_id)
+    tok_list1[mask1_index] = top_pred
+    a_complete = TOKENIZER.decode(tok_list1[1:]) # remove <s> (let </s> stay)
+    ic(a_complete)
+
+    # making A pred <sep> A' <mask>
+    AAbar = a_complete + inp[1]
+    ic(AAbar)
+    return compare_n_sentences(model, [inp[0], inp[1], AAbar], inp[2])
+
+
+def deal_with_pair_expt3(model, inp):
+    """
+    input will have "A <mask>" and <A' mask>
+    - We will first create "A <pred>"
+    - We will then make A <pred>. <sep> <A'> <mask>
+    - Compare against A <mask>. predictions and A' <mask> predictions
+    """
+    k = 20
+    # completing A<mask>
+    masked_logits1 = send_mask_logits(model, inp[0])
+    softmax_func = Softmax()
+    preds1_prob = softmax_func(masked_logits1.squeeze())
+    preds1, indices1 = top_k(k, preds1_prob)
+    top_pred = indices1[0]
+    tok_list1 = TOKENIZER(inp[0]).input_ids
+    mask1_index = tok_list1.index(TOKENIZER.mask_token_id)
+    tok_list1[mask1_index] = top_pred
+    a_complete = TOKENIZER.decode(tok_list1[1:-1]) # remove <s> and </s>
+    ic(a_complete)
+
+    # making A pred <sep> A' <mask>
+    AAbar = a_complete + " " + inp[1]
+    ic(AAbar)
+    return compare_n_sentences(model, [inp[0], inp[1], AAbar], inp[2])
+
+
 
 def main():
     model = RobertaForMaskedLM.from_pretrained(
@@ -169,8 +274,14 @@ def main():
     dataloader = DataLoader(dataset, batch_size=1)
     # iter_dataloader = iter(dataloader)
 
-    for inp in dataloader:
-        print(deal_with_pair(model, inp))
-        print("\n")
+    f1 = open("with_sep_experiment.txt", "w")
+    f2 = open("without_sep_experiment.txt", "w")
+    for i in range(len(dataset)):
+        f1.write(deal_with_pair_expt2(model, dataset[i]))
+        f1.flush()
+        f2.write(deal_with_pair_expt3(model, dataset[i]))
+        f2.flush()
 
+    f1.close()
+    f2.close()
 main()
